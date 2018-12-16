d3e17ad8738013c586c002819c5aff68897884e3
Robert Novotny <novotnr0@gmail.com>
2018-12-16 16:02:50 +0100

    Refactor structure of Configuration Factories and Configuration Types

--- src/com/github/novotnyr/idea/rabbitmq/RabbitMqScriptConfigurationType.java
+++ src/com/github/novotnyr/idea/rabbitmq/RabbitMqScriptConfigurationType.java
@@ -1,36 +1,48 @@
 package com.github.novotnyr.idea.rabbitmq;
 
 import com.intellij.execution.configurations.ConfigurationFactory;
-import com.intellij.execution.configurations.ConfigurationTypeBase;
+import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.ConfigurationTypeUtil;
-import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.openapi.project.Project;
+import com.intellij.icons.AllIcons;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.Icon;
 
-public class RabbitMqScriptConfigurationType extends ConfigurationTypeBase {
-
-    private final ConfigurationFactory factory;
-
-    protected RabbitMqScriptConfigurationType() {
-        super("com.github.novotnyr.rabbitmq.RabbitMqScriptConfigurationType", "RabbitMQ", "Run RabbitMQ", (Icon) null);
-        this.factory = new ConfigurationFactory(this) {
-            @NotNull
-            @Override
-            public RunConfiguration createTemplateConfiguration(@NotNull Project project) {
-                RabbitMqScriptRunConfiguration runConfiguration = new RabbitMqScriptRunConfiguration(project, this, "");
-                return runConfiguration;
-            }
-        };
-        addFactory(this.factory);
-    }
+public class RabbitMqScriptConfigurationType implements ConfigurationType {
 
     public static RabbitMqScriptConfigurationType getInstance() {
         return ConfigurationTypeUtil.findConfigurationType(RabbitMqScriptConfigurationType.class);
     }
 
-    public ConfigurationFactory getFactory() {
-        return this.factory;
+    @Override
+    public String getDisplayName() {
+        return "RabbitMQ Script";
+    }
+
+    @Override
+    public String getConfigurationTypeDescription() {
+        return "Run RabbitMQ Script";
+    }
+
+    @Override
+    public Icon getIcon() {
+        return AllIcons.RunConfigurations.Unknown;
+    }
+
+    @NotNull
+    @Override
+    public String getId() {
+        return "com.github.novotnyr.rabbitmq.RabbitMqScriptConfigurationType";
+    }
+
+    @Override
+    public ConfigurationFactory[] getConfigurationFactories() {
+        return new ConfigurationFactory[]{
+                new RabbitMqScriptConfigurationFactory(this)
+        };
+    }
+
+    public RabbitMqScriptConfigurationFactory getFactory() {
+        return (RabbitMqScriptConfigurationFactory) getConfigurationFactories()[0];
     }
 }
